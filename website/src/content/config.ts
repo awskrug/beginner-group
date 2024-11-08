2f189f37d51bd581b19a6b09a1bab4344454d16c
naru200 <jinsu2504@gmail.com>
2024-11-08 14:42:37 +0900

    Add new session data, Edit some UI

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -1,14 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
 const sessions = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    speaker: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string().optional(),
+		speaker: z.string(),
+		speakerImageURL: z.string().optional(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 export const collections = { sessions };
