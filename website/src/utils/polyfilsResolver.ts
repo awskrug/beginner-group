2cbced9b89dd72ed7f161edd62101d62be043cae
naru200 <jinsu2504@gmail.com>
2024-11-11 17:16:56 +0900

    Ignore biome error

--- website/src/utils/polyfilsResolver.ts
+++ website/src/utils/polyfilsResolver.ts
@@ -1,23 +1,27 @@
 if (typeof Promise.withResolvers === "undefined") {
-  if (typeof window !== "undefined") {
-    // @ts-expect-error This does not exist outside of polyfill which this is doing
-    window.Promise.withResolvers = function () {
-      let resolve, reject;
-      const promise = new Promise((res, rej) => {
-        resolve = res;
-        reject = rej;
-      });
-      return { promise, resolve, reject };
-    };
-  } else {
-    // @ts-expect-error This does not exist outside of polyfill which this is doing
-    global.Promise.withResolvers = function () {
-      let resolve, reject;
-      const promise = new Promise((res, rej) => {
-        resolve = res;
-        reject = rej;
-      });
-      return { promise, resolve, reject };
-    };
-  }
+	if (typeof window !== "undefined") {
+		// @ts-expect-error This does not exist outside of polyfill which this is doing
+		window.Promise.withResolvers = () => {
+			// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
+			// biome-ignore lint/style/useSingleVarDeclarator: <explanation>
+			let resolve, reject;
+			const promise = new Promise((res, rej) => {
+				resolve = res;
+				reject = rej;
+			});
+			return { promise, resolve, reject };
+		};
+	} else {
+		// @ts-expect-error This does not exist outside of polyfill which this is doing
+		global.Promise.withResolvers = () => {
+			// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
+			// biome-ignore lint/style/useSingleVarDeclarator: <explanation>
+			let resolve, reject;
+			const promise = new Promise((res, rej) => {
+				resolve = res;
+				reject = rej;
+			});
+			return { promise, resolve, reject };
+		};
+	}
 }
