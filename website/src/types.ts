0838fffde95c3f5658ccc53b9e0e0d1ee26ef351
naru200 <jinsu2504@gmail.com>
2025-01-14 14:52:53 +0900

    Apply biome

--- website/src/types.ts
+++ website/src/types.ts
@@ -1,17 +1,17 @@
 export type Site = {
-  NAME: string;
-  EMAIL: string;
-  NUM_POSTS_ON_HOMEPAGE: number;
-  NUM_WORKS_ON_HOMEPAGE: number;
-  NUM_PROJECTS_ON_HOMEPAGE: number;
+	NAME: string;
+	EMAIL: string;
+	NUM_POSTS_ON_HOMEPAGE: number;
+	NUM_WORKS_ON_HOMEPAGE: number;
+	NUM_PROJECTS_ON_HOMEPAGE: number;
 };
 
 export type Metadata = {
-  TITLE: string;
-  DESCRIPTION: string;
+	TITLE: string;
+	DESCRIPTION: string;
 };
 
 export type Socials = {
-  NAME: string;
-  HREF: string;
+	NAME: string;
+	HREF: string;
 }[];
